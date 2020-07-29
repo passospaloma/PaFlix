2699ad69b702e0b2f930d84c26a20888a1a9e6de
PassosPaloma <passos.paloma@hotmail.com>
2020-07-29 11:10:55 -0300

    initial setup

---
+++ src/components/Menu/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
+
+export default function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Paflix logo" />
+      </a>
+
+      <Button as="a" className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
+}
