eb9ad4dd2e048e61a2410885df5fb2077adf74f2
PassosPaloma <passos.paloma@hotmail.com>
2020-07-30 21:11:30 -0300

    commit hoje

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,10 +10,13 @@ function Home() {
     <div style={{ background: '#141414' }}>
       <Menu />
       <BannerMain
-        videoTitle={dadosIniciais.categorias[0].video[0].titulo[0]}
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={'O que é Front-end? Trabalhando na área'}
+        videoDescription={
+          'O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!'
+        }
       />
+
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[1]} />
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[2]} />
